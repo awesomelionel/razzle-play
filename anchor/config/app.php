f6590a005f99074c0252137f6b61d9d9363e63de
lionel@razzle.io <lionel@razzle.io>
2013-09-24 00:31:35 +0800

    Uploaded on Dazzle

---
+++ anchor/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'url' => '/blog',
+	'index' => 'index.php',
+	'timezone' => 'Asia/Kuala_Lumpur',
+	'key' => 'yGiQsJfHWDgeqXXS5tL9kH8p2ERmuQ3J',
+	'language' => 'en_GB',
+	'encoding' => 'UTF-8'
+);
